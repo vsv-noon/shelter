2b5a9a3546b97a237791c51bfe776448a57e6516
ViacheslavAnisimov <forvsn@gmail.com>
2024-08-27 11:42:06 +0700

    feat: add pets.json

--- shelter/js/script.js
+++ shelter/js/script.js
@@ -1,3 +1,5 @@
+// import { pets } from "../data/pets";
+
 const hamburgerMenu = document.querySelector('.hamburger-button');
 const navMenu = document.querySelector('.nav-menu-list');
 const overlay = document.querySelector('.overlay');
@@ -9,7 +11,7 @@ window.addEventListener('resize', () => {
   hamburgerMenu.classList.remove('hamburger-active');
   navMenu.classList.remove('nav-menu-active');
   overlay.classList.remove('overlay-on');
-})
+});
 
 hamburgerMenu.addEventListener('click', () => {
   hamburgerMenu.classList.toggle('hamburger-active');
@@ -23,32 +25,33 @@ const overlayOn = () => {
   overlayOn.classList.add('overlay-on');
   document.body.appendChild(overlayOn);
   document.body.style.overflow = 'hidden';
-}
+};
 
 const overlayOff = () => {
   document.querySelector('.modal').remove();
-       document.querySelector('.overlay-on').remove();
-       document.body.style.overflow = '';
-}
+  document.querySelector('.overlay-on').remove();
+  document.body.style.overflow = '';
+};
 
 document.addEventListener('click', (e) => {
   console.log(e.target);
-  if (!navMenu.contains(e.target)
-    && e.target !== hamburgerMenu
-    || e.target.classList.contains('nav-link')
-    || e.target.classList.contains('nav-active')) {
+  if (
+    (!navMenu.contains(e.target) && e.target !== hamburgerMenu) ||
+    e.target.classList.contains('nav-link') ||
+    e.target.classList.contains('nav-active')
+  ) {
     document.body.classList.remove('lock');
     hamburgerMenu.classList.remove('hamburger-active');
     navMenu.classList.remove('nav-menu-active');
     overlay.classList.remove('overlay-on');
   }
 
-     let target = e.target.closest('.modal-close-btn');
-     let classes = e.target.classList;
+  let target = e.target.closest('.modal-close-btn');
+  let classes = e.target.classList;
 
-     if (target || classes.contains('overlay-on')) {
-       overlayOff();
-     }
+  if (target || classes.contains('overlay-on')) {
+    overlayOff();
+  }
 });
 
 // Modal
@@ -56,16 +59,47 @@ document.addEventListener('click', (e) => {
 const ourFriendsCard = document.querySelectorAll('.our-friends-card');
 const closeButton = document.querySelector('.modal-close-btn');
 
-function createElementPopup() {
+let cards = null;
+
+fetch('./data/pets.json')
+  .then((response) => response.json())
+  .then((data) => {
+    cards = data;
+    console.log(cards);
+    addDataToHTML();
+  });
+
+function addDataToHTML() {
+  let listCardsHTML = document.querySelector('.slider-wrapper');
+
+  if (cards != null) {
+    cards.forEach((card) => {
+      let newCard = document.createElement('div');
+      // let link = document.createElement('a');
+      // link.href = 'index.html?id=' + card.id;
+      // newCard.appendChild(link);
+      // newCard.href = 'index.html?id=' + card.id;
+      // newCard = document.createElement('a');
+      newCard.classList.add('slide');
+      newCard.classList.add('our-friends-card');
+      newCard.innerHTML = `<img class="slide-img" src="${card.img}" alt="${card.name}">
+         <h4 class="card-title">${card.name}</h4>
+         <button class="slide-button">Learn more</button>`;
+      listCardsHTML.appendChild(newCard);
+    });
+  }
+}
+
+function createElementPopup(card) {
   const modalCard = document.createElement('div');
   modalCard.classList.add('modal');
   modalCard.innerHTML = `
     <button class="modal-close-btn">
-      <img src="./icons/close-btn.svg" alt="close-button">
+      <img src="./assets/icons/close-btn.svg" alt="close-button">
     </button>
-    <img class="modal-img" src="./images/pets-jennifer.png" alt="">
+    <img class="modal-img" src="./assets/images/pets-jennifer.png" alt="">
     <div class="modal-info">
-      <h3 class="modal-title">Jennifer</h3>
+      <h3 class="modal-title"></h3>
       <h4 class="modal-subtitle">Dog - Labrador</h4>
       <p class="modal-description">
         Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.
@@ -76,20 +110,25 @@ function createElementPopup() {
         <li><span>Diseases:</span> none</li>
         <li><span>Parasites:</span> none</li>
       </ul>
-    </div>  `
+    </div>  `;
 
   document.body.appendChild(modalCard);
 }
 
-ourFriendsCard.forEach((el) => {
-  el.addEventListener('click', () => {
+console.log(ourFriendsCard)
+
+// ourFriendsCard.forEach((el) => {
+//   el.addEventListener('click', () => {
+//     overlayOn();
+//     createElementPopup();
+//   });
+// });
+
+document.addEventListener('click', (e) => {
+  let target = e.target.closest('.slide');
+  if (target) {
+    e.preventDefault();
     overlayOn();
     createElementPopup();
-  });
-
+  }
 })
-
-
-
-
-
